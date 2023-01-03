93a1cc2bac38af03882ea2c60c94abbcb483c0e1
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-03 09:52:09 +0100

    exo2

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 //EXO1
-function addition(x, y) {
-    let resultat = x+y;
+// function addition(x, y) {
+//     let resultat = x+y;
+//     console.log(resultat);
+    
+// }
+// addition(5,5);
+
+
+//EXO2
+function soustraction(x,y) {
+    let resultat = y-x;
     console.log(resultat);
     
 }
-addition(5,5);
+
+soustraction(5,20);
